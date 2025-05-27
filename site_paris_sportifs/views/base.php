187c9747b6005454afbc922556cf7384432ea078
Maxou-Maxou <maxencekestelyn07@gmail.com>
2025-05-27 09:49:19 +0200

    Charte graphique
    Ajoute palettes couleurs
    Ajout DA.css dans base
    
    Vous avez juste à mettre le nom des variable de DA pour vos css.

--- site_paris_sportifs/views/base.php
+++ site_paris_sportifs/views/base.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="public/images/placeholderIcon.png">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="public/CSS/base_style.css">
+    <link rel="stylesheet" href="public/css/DA.css">
+    <link rel="stylesheet" href="public/css/base_style.css">
 </head>
+
 <body>
     <!-- C'est un fichier template qui sert
     de base structurelle commune à toutes les pages qui se ressembleront-->
