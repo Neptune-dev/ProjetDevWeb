cdd47e336a058141eb0e3308bb51fecbce829b2d
Maxou-Maxou <maxencekestelyn07@gmail.com>
2025-05-27 14:05:56 +0200

    Ajout contact et footer dans homepage
    Ajout contact
    Ajout footer dans homerpage
    Avec le CSS des 2, peut-être à rendre plus propre.
    
    Remplacement du nom 'background-color' par "bg-color" pour simplifier l'utilisation.

--- site_paris_sportifs/views/homepage.php
+++ site_paris_sportifs/views/homepage.php
@@ -7,6 +7,8 @@
     <title>Home page</title>
     <link rel="stylesheet" href="public/css/DA.css">
     <link rel="stylesheet" href="public/css/homepage_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!--Le dernier c'est pour les favicons de entreprises pour le footer-->
 </head>
 <body>
     <header>
@@ -21,6 +23,37 @@
             </ul>
         </nav>
     </header>
+    <button onclick="toggleTheme()">Dark Mode 🌓</button>
+    <section id="Contact">
+        <hr>
+        <h2>Contactez-moi</h2>
+        <form id="contactForm">
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom" required>
+
+            <label for="email">Email :</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="message">Message :</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+
+            <button type="submit">Envoyer</button>
+        </form>
+        <p id="msg"></p>
+    </section>
+    <footer>
+        <div>
+            <p><a href="https://www.conseil-constitutionnel.fr/le-bloc-de-constitutionnalite/declaration-des-droits-de-l-homme-et-du-citoyen-de-1789" target="_blank">Mentions légales</a></p>
+            <div class="socials">
+                <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <p>&copy; <span id="year"></span> Groupe_des_Wapitis</p>
+        </div>
+    </footer>
+    <script>document.getElementById("year").textContent = new Date().getFullYear();</script>
+
     <?php
     session_start();
     ?>
