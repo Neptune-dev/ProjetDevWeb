94099cc151abfc2fd4f5e63f7e27eb854602a3aa
Maxou-Maxou <maxencekestelyn07@gmail.com>
2025-05-27 10:38:21 +0200

    Les fonts
    Ajout des fonts
    Ajout des fonts automatiquement via h1 etc. pour la homepage.

--- site_paris_sportifs/views/homepage.php
+++ site_paris_sportifs/views/homepage.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="public/images/placeholderIcon.png">
     <title>Home page</title>
-    <link rel="stylesheet" href="public/CSS/homepage_style.css">
+    <link rel="stylesheet" href="public/css/DA.css">
+    <link rel="stylesheet" href="public/css/homepage_style.css">
 </head>
 <body>
     <h1>Bienvenue</h1>
+    <p> Alors est-ce que le Monserrat marche ?</p>
     <?php
     session_start();
     ?>
