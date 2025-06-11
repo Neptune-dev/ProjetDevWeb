51724f6e858bf56cc2eca9f4a2ef9c887649157e
Neptune <felix@ladislas.fr>
2025-06-11 15:20:28 +0200

    Ajout desequipes de ligue 1
    - mise à jour de DBsetup

--- BDsetup.sql
+++ BDsetup.sql
@@ -84,25 +84,68 @@ WHERE Username="DarkTOTO";
 INSERT INTO Users (FirstName, LastName, Username, Birthdate, Pwd, isAdmin)
 VALUES ("admin", "admin", "admin",  "2000-01-01", "$2y$10$dxFZ3Cv.kyxBGgEG0UfGl..0D66Glj5q5Spr6QQdjRz6ex/jhHRrm", 1);
 
-/* Equipes d'exemples */
+/* Equipes d'exemples (On a pris celles de la ligue 1*/
 INSERT INTO Teams (TeamName, TeamLogo)
-VALUES ("PSG", "https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/2048px-Paris_Saint-Germain_Logo.svg.png");
+VALUES ("Paris-SG", "https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/2048px-Paris_Saint-Germain_Logo.svg.png");
 
 INSERT INTO Teams (TeamName, TeamLogo)
-VALUES ("OM", "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1612px-Logo_Olympique_de_Marseille.svg.png");
+VALUES ("Marseille", "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1612px-Logo_Olympique_de_Marseille.svg.png");
 
 INSERT INTO Teams (TeamName, TeamLogo)
-VALUES ("OL", "https://upload.wikimedia.org/wikipedia/fr/thumb/a/a5/Logo_Olympique_Lyonnais_-_2022.svg/1317px-Logo_Olympique_Lyonnais_-_2022.svg.png");
+VALUES ("Lyon", "https://upload.wikimedia.org/wikipedia/fr/thumb/a/a5/Logo_Olympique_Lyonnais_-_2022.svg/1317px-Logo_Olympique_Lyonnais_-_2022.svg.png");
 
 INSERT INTO Teams (TeamName, TeamLogo)
 VALUES ("Monaco", "https://upload.wikimedia.org/wikipedia/fr/thumb/5/58/Logo_AS_Monaco_FC_-_2021.svg/1200px-Logo_AS_Monaco_FC_-_2021.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Nice", "https://upload.wikimedia.org/wikipedia/fr/thumb/b/b1/Logo_OGC_Nice_2013.svg/1200px-Logo_OGC_Nice_2013.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Lille", "https://upload.wikimedia.org/wikipedia/fr/thumb/6/62/Logo_LOSC_Lille_2018.svg/1200px-Logo_LOSC_Lille_2018.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Strasbourg", "https://upload.wikimedia.org/wikipedia/fr/thumb/7/70/Racing_Club_de_Strasbourg_Alsace_%28RC_Strasbourg_-_RCS_-_RCSA%29_logo_officiel.svg/1200px-Racing_Club_de_Strasbourg_Alsace_%28RC_Strasbourg_-_RCS_-_RCSA%29_logo_officiel.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Lens", "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c5/Logo_RC_Lens.svg/1200px-Logo_RC_Lens.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Brest", "https://upload.wikimedia.org/wikipedia/fr/thumb/1/14/Logo_Stade_Brestois.svg/1200px-Logo_Stade_Brestois.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Toulouse", "https://upload.wikimedia.org/wikipedia/fr/thumb/8/8b/Logo_Toulouse_FC_2018.svg/1200px-Logo_Toulouse_FC_2018.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Auxerre", "https://upload.wikimedia.org/wikipedia/fr/thumb/f/f6/Logo_AJ_Auxerre_2024.svg/langfr-250px-Logo_AJ_Auxerre_2024.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Rennes", "https://upload.wikimedia.org/wikipedia/fr/thumb/e/e9/Logo_Stade_Rennais_FC.svg/1200px-Logo_Stade_Rennais_FC.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Nantes", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Logo_FC_Nantes_%28avec_fond%29_-_2019.svg/1200px-Logo_FC_Nantes_%28avec_fond%29_-_2019.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Angers", "https://upload.wikimedia.org/wikipedia/fr/thumb/d/d3/Logo_Angers_SCO_-_2021.svg/1720px-Logo_Angers_SCO_-_2021.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Le Havre", "https://upload.wikimedia.org/wikipedia/fr/thumb/e/e6/Logo_Havre_AC_2012.svg/1200px-Logo_Havre_AC_2012.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Reims", "https://upload.wikimedia.org/wikipedia/fr/thumb/a/ad/Logo_Stade_de_Reims_2020_%28Alternatif%29.svg/1200px-Logo_Stade_de_Reims_2020_%28Alternatif%29.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Saint-Etienne", "https://upload.wikimedia.org/wikipedia/fr/thumb/3/3e/AS_Saint-Etienne-logo_2022.svg/1642px-AS_Saint-Etienne-logo_2022.svg.png");
+
+INSERT INTO Teams (TeamName, TeamLogo)
+VALUES ("Montpellier", "https://upload.wikimedia.org/wikipedia/commons/6/6a/Logo_-_Montpellier_H%C3%A9rault_Sport_Club_%28MHSC%29.png");
+
 
 /* matchs d'exemple */
 INSERT INTO Games (League, Home, Away, GameDate, GameTime, HomeOdd, AwayOdd, HomeDynaOdd, DrawDynaOdd, AwayDynaOdd)
 VALUES (
 "Ligue 1",
-(SELECT ID FROM Teams WHERE TeamName = "PSG"),
-(SELECT ID FROM Teams WHERE TeamName = "OM"),
+(SELECT ID FROM Teams WHERE TeamName = "Paris-SG"),
+(SELECT ID FROM Teams WHERE TeamName = "Marseille"),
 "2025-06-15",
 "20:45:00",
 1.5,
@@ -112,7 +155,7 @@ VALUES (
 INSERT INTO Games (League, Home, Away, GameDate, GameTime, HomeOdd, AwayOdd, HomeDynaOdd, DrawDynaOdd, AwayDynaOdd)
 VALUES (
 "Ligue 1",
-(SELECT ID FROM Teams WHERE TeamName = "OL"),
+(SELECT ID FROM Teams WHERE TeamName = "Lyon"),
 (SELECT ID FROM Teams WHERE TeamName = "Monaco"),
 "2025-06-16",
 "21:00:00",
